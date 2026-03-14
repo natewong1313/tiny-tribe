eaf8ffe81ca15c7f6d59e83802161e8feb3f18d3
natewong1313 <natewong1@gmail.com>
2026-03-14 17:35:08 -0500

    feat: add avatars bucket, fix tsconfig

--- src/data/models.cloesce.ts
+++ src/data/models.cloesce.ts
@@ -1,9 +1,15 @@
-import { Integer, Model, WranglerEnv } from "cloesce/backend";
-import { D1Database } from "@cloudflare/workers-types";
+import { Inject, Integer, Model, PUT, R2, WranglerEnv } from "cloesce/backend";
+import {
+  D1Database,
+  R2Bucket,
+  R2ObjectBody,
+  ReadableStream,
+} from "@cloudflare/workers-types";
 
 @WranglerEnv
 export class Env {
   db!: D1Database;
+  bucket!: R2Bucket;
 }
 
 @Model(["SAVE", "GET", "LIST"])
@@ -11,4 +17,12 @@ export class User {
   id!: Integer;
   username!: string;
   email!: string;
+
+  @R2("user/avatars/{id}.png", "bucket")
+  avatar!: R2ObjectBody;
+
+  @PUT
+  async uploadAvatar(@Inject env: Env, stream: ReadableStream) {
+    await env.bucket.put(`user/avatars/${this.id}.png`, stream);
+  }
 }
